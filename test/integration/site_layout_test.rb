cb2d3db87ef0eaf0ee6d86ab90ba258c72d883a7
kuimac <kumagaikousuke1995@gmail.com>
2017-01-22 13:34:22 +0000

    next 5.4 ユーザー登録: 最初のステップ

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
 test "layouts links" do
   get root_path
   assert_template 'static_pages/home'
@@ -8,6 +12,8 @@ test "layouts links" do
   assert_select "a[href=?]", help_path
   assert_select "a[href=?]", about_path
   assert_select "a[href=?]", contact_path
+  get contact_path
+   assert_select "title", full_title("Contact")
 end
  
 end
