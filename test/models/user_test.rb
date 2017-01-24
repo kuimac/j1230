71fb3c2131fc19a34a8b99c95bf226cd9692c7e5
kuimac <kumagaikousuke1995@gmail.com>
2017-01-24 17:40:24 +0000

    next 6.2.3 長さを検証する6.2.3 長さを検証する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,15 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+  
+  test "name should be present"  do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  
+  test "e,mail should be present"  do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+  
 end
