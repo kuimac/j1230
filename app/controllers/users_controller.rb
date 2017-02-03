6e8d995a568cfc9cc8a5639ea985d49d0e4d2165
kuimac <kumagaikousuke1995@gmail.com>
2017-02-03 04:01:04 +0000

    next 7.3.4 失敗時のテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    #@user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
     else
       render 'new'
     end
+  end
+  
+  private
+  
+  def user_params #悪意ある攻撃を防ぐ
+    params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
